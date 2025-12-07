4d163faac79e1a97b5d427a02cca961fde3de5b6
Tomas Roun <tomas.roun8@gmail.com>
2025-12-07 12:13:35 +0100

    Bump version

--- pivotal/__init__.py
+++ pivotal/__init__.py
@@ -3,5 +3,5 @@ from pivotal.errors import AbsoluteValueRequiresMILP, Infeasible, Unbounded
 from pivotal.expressions import Variable
 
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __all__ = ["AbsoluteValueRequiresMILP", "Infeasible", "Unbounded", "Variable", "maximize", "minimize"]
